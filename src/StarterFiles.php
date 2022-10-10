de15facb4f1cf45e3bb9612968b7c86fce785cee
Ian Low <ianlow27@gmail.com>
2022-10-10 14:09:56 +0100

    #221010d: ychwanegwd .gitignore, a chod html yn y prifddosbarth phpffeil main().

--- src/StarterFiles.php
+++ src/StarterFiles.php
@@ -129,15 +129,66 @@ DONE:
 namespace '. $lvendor. '\\'. $lnamespace. ';
 class '. $lnamespace . '{
 
-  //-------------------------------
-  public function __construct(){
-    echo "This is called from '. $lnamespace. '->__construct()\n";
-  }//endfunc
-  //-------------------------------
-  public function main(){
-    echo "This is called from '. $lnamespace. '->main()\n";
-  }//endfunc
-  //-------------------------------
+//-------------------------------
+public function __construct(){
+  echo "This is called from '. $lnamespace. '->__construct()\n";
+}//endfunc
+//-------------------------------
+public function testfunc($pstr){
+  echo "[parameter passed into testfunc(): ". $pstr ." ]";
+}//endfunc
+//-------------------------------
+public function main(){
+echo \'<!DOCTYPE html><html><head><meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+__a {text-decoration:none;}
+</style>
+</head>
+<body style="margin:0px;font-family:Times New Roman;">
+<table width="100%" border=0 borderwidth=0 bordercolor=blue cellspacing=0 cellpadding=0>
+<tr><td style="background-color:darkslategrey;color:white;xxtext-align:right;">
+<table width="100%">
+<tr>
+<td width="99%" style="text-align:center;" >
+  php-slideshow
+</td>
+<td width="1%" >
+<input type="text" onkeydown="txcmd_onkeypress();" id="txcmd" style="width:10px;"/>
+</td>
+</tr>
+</table>
+
+<script>
+let txcmd = document.getElementById("txcmd");
+txcmd.focus();
+function txcmd_onkeypress(){
+  let e=event;
+  const keycode = Number(e.keyCode);
+  if      ((keycode == 39) || (keycode == 40)){
+    alert("forward");
+  }else if((keycode == 37) || (keycode == 38)){
+    alert("backward");
+  }
+txcmd.value="";
+txcmd.focus();
+}//endfunc
+
+</script>
+
+
+</td></tr>
+<tr><td>\';
+
+  echo "This is called from '. $lnamespace. '->main()\n";
+  echo self::testfunc(date("Ymd-Hi:s"));
+
+echo \'
+</td></tr>
+</table>
+</body></html>\';
+}//endfunc
+//-------------------------------
 
 }//endclass 
 ?>
@@ -162,6 +213,11 @@ $oprawf1->main();
 ?>
 ');
 echo "created ./src/test.php!\n";
+       }//endif
+       //-----------------------------------------
+       if(!file_exists("./.gitignore")){
+         file_put_contents("./.gitignore", 'vendor');
+echo "created ./.gitignore!\n";
        }//endif
        //-----------------------------------------
        if(!file_exists("./src/servephp.sh")){
